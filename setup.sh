1f84a80e461614e6628eb5567f59b8d6a253b18f
indocoding-ID <indocoding.id@gmail.com>
2024-05-18 22:29:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # update package
-pkg update && pkg upgrade
+pkg update && pkg upgrade -y
 # install tor repo
-pkg install tur-repo
+pkg install tur-repo -y
 # install code-server
-pkg install code-server
+pkg install code-server -y
+# install openssl
+pkg install openssl -y
+# setup comman# create started
+cat << EOF > /data/data/com.termux/files/usr/bin/vs
+#!/bin/sh
+nohup code-server --bind-addr 0.0.0.0:2121 --auth none --cert >/dev/null 2>&1 &
+EOF
+
+echo "kini anda dapat menjalankan code-server cuku ketik command 'vs' dan kemudiab enter";
